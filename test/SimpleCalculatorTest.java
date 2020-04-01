fe594a108b7c6850ac43138c606ddafc9260a8a0
Jeyner Gil Caga <jeynergilcaga@gmail.com>
2020-04-01 14:13:43 +0800

    added test cases for add, mul, div, sub

--- test/SimpleCalculatorTest.java
+++ test/SimpleCalculatorTest.java
@@ -4,27 +4,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleCalculatorTest {
 
+    private int x = 10;
+    private int y = 10;
+
+    SimpleCalculator sc = new SimpleCalculator();
 
     @Test
     void add() {
-
+        assertEquals(20, sc.add(x,y));
     }
 
     @Test
     void sub() {
-
+        assertEquals(0, sc.sub(x,y));
     }
 
     @Test
     void div() {
+        assertEquals(1, sc.div(x,y));
     }
 
     @Test
     void mul() {
+        assertEquals(100, sc.mul(x,y));
     }
 
     @Test
     void clear() {
+
     }
 
     @Test
