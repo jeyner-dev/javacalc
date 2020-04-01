182917092c5213be331065aa5422a3ace7b2ea02
Jeyner Gil Caga <jeynergilcaga@gmail.com>
2020-04-01 13:55:27 +0800

    added test framework for SimpleCalculator

---
+++ test/SimpleCalculatorTest.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleCalculatorTest {
+
+
+    @Test
+    void add() {
+
+    }
+
+    @Test
+    void sub() {
+
+    }
+
+    @Test
+    void div() {
+    }
+
+    @Test
+    void mul() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void version() {
+    }
+
+    @Test
+    void on() {
+    }
+
+    @Test
+    void off() {
+    }
+}
