36c527bca9da4c6c9ea00319aab5000b0900d480
Jeyner Gil Caga <jeynergilcaga@gmail.com>
2020-04-01 11:48:08 +0800

    converted to interface

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,29 +1,10 @@
-public abstract class Calculator {
-    private int x;
-    private int y;
+public interface Calculator {
 
-    public Calculator(int x, int y) {
-        this.x = x;
-        this.y = y;
-        System.out.println("Base Calculator Called");
-    }
-
-    public float add() {
-        return x + y;
-    }
-    public float sub() {
-        return x - y;
-    }
-    public float div() {
-        return (float)x / y;
-    }
-    public float mul() {
-        return x * y;
-    }
-    public void clear() {
-        x = 0;
-        y = 0;
-    }
+    public abstract float add(int x, int y);
+    public abstract float sub(int x, int y);
+    public abstract float div(int x, int y);
+    public abstract float mul(int x, int y);
+    public abstract void clear();
     public abstract void version();
     public abstract void on();
     public abstract void off();
