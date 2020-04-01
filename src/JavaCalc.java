b9453456e9dc9bcaab0c77ee8083834d30156631
Jeyner Gil Caga <jeynergilcaga@gmail.com>
2020-04-01 09:33:45 +0800

    refactor code by creating SimpleCalculator.java.

--- src/JavaCalc.java
+++ src/JavaCalc.java
@@ -1,22 +1,11 @@
 public class JavaCalc {
-    public static float add(int x, int y) {
-        return x + y;
-    }
-    public static float sub(int x, int y) {
-        return x - y;
-    }
-    public static float div(int x, int y) {
-        return (float)x / y;
-    }
-    public static float mul(int x, int y) {
-        return x * y;
-    }
+
     public static void main(String[] args) {
         int x = 10;
         int y = 10;
-        System.out.println("Add: "+ add(x,y));
-        System.out.println("Div: "+ div(x,y));
-        System.out.println("Sub: "+ sub(x,y));
-        System.out.println("Mul: "+ mul(x,y));
+        System.out.println("Add: "+ SimpleCalculator.add(x,y));
+        System.out.println("Div: "+ SimpleCalculator.div(x,y));
+        System.out.println("Sub: "+ SimpleCalculator.sub(x,y));
+        System.out.println("Mul: "+ SimpleCalculator.mul(x,y));
     }
 }
